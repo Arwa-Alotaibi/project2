26b6d01d81dcd98bd9512fe080477e91523b0089
Arwa <88631496+Arwa-Alotaibi@users.noreply.github.com>
2023-01-22 18:13:46 +0300

    Add files via upload

--- Project2/src/Airplane.java
+++ Project2/src/Airplane.java
@@ -1,69 +1,15 @@
 public class Airplane extends  Trip{
-    double fuel;
-    double miles;
-    double gallons;
-    double lpk;
-    double mpg;
 
-    public Airplane(String tripNumber, double distance, double speed, String startTime , double fuel, double gallons , double miles ,double mpg
+
+    public Airplane(String tripNumber, double distance, double speed, String startTime
     ) {
         super(tripNumber, distance, speed, startTime);
-        this.fuel= fuel;
-        this.gallons = gallons;
-        this.miles=miles;
-        this.mpg=mpg;
+
     }
 
-    public double getFuel() {
-        return fuel;
-    }
 
-    public double getMiles() {
-        return miles;
-    }
 
-    public double getGallons() {
-        return gallons;
-    }
 
-    public double getLpk() {
-        return lpk;
-    }
-
-    public double getMpg() {
-        return mpg;
-    }
-
-    public void setFuel(double fuel) {
-        this.fuel = fuel;
-    }
-
-    public void setMiles(double miles) {
-        this.miles = miles;
-    }
-
-    public void setGallons(double gallons) {
-        this.gallons = gallons;
-    }
-
-    public void setLpk(double lpk) {
-        this.lpk = lpk;
-    }
-
-    public void setMpg(double mpg) {
-        this.mpg = mpg;
-    }
-
-    public  void Fuel_Consumption_Calculator(){
-        lpk = (fuel/super.getDistance());
-        miles = super.getDistance()*0.6214;
-        gallons = fuel*0.2642;
-        mpg=miles/gallons;
-        String lpk1=String.format("%.2f",lpk);
-        String mpg1=String.format("%.2f",mpg);
-        System.out.println("Liters/100KM"+lpk1);
-        System.out.println("Miles/gallons"+mpg1);
-    }
     public int CalculateDuration() {
         double time = super.getDistance()/super.getSpeed();
         // Converting Double data to String data
--- Project2/src/Main.java
+++ Project2/src/Main.java
@@ -4,10 +4,9 @@ public class Main {
 
         System.out.println("Hello world!");
 
-       Car c = new Car("11",171,12,"11:00",12.5,11.6,11.2,44.6);
-        Airplane a = new Airplane("11",171,12.3,"11:00",44.44,42.4,33,33);
+       Car c = new Car("11",171,12,"11:00");
+        Airplane a = new Airplane("11",171,12.3,"11:00");
         System.out.println(c.convertMinToHour(120));
-        c.Fuel_Consumption_Calculator();
          c.CalculateDuration();
          a.CalculateDuration();
 
